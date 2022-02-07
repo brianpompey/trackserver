516a5f2bdc8546ba42d7e926a536287b5383544d
brianpompey <brianthescholar@gmail.com>
2022-02-06 20:57:08 -0500

    using track routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-require('./models/User')
+require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 
@@ -10,6 +12,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.6lm5w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
