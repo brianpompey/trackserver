f0fcad31a1410df78622cb50605d18e350c230a5
brianpompey <brianthescholar@gmail.com>
2022-02-06 21:15:15 -0500

    fixed the issue

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireAuth = require('../middlewares/requireAuth');
 
-
 const Track = mongoose.model('Track');
 
 const router = express.Router();
@@ -10,10 +9,27 @@ const router = express.Router();
 router.use(requireAuth);
 
 router.get('/tracks', async (req, res) => {
-    const tracks = await Track.find({ userId: req.user._id });
+  const tracks = await Track.find({ userId: req.user._id });
 
-    res.send(tracks);
+  res.send(tracks);
 });
 
+router.post('/tracks', async (req, res) => {
+  const { name, locations } = req.body;
+
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and locations' });
+  }
+
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    res.send(track);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
 
 module.exports = router;
